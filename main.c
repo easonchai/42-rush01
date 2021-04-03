aad56a7eaab250592fb14a439dc2c5c63daa1188
Eason Chai <echai@student.42kl.edu.my>
2021-04-03 10:55:17 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echai <echai@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/03 10:01:34 by echai             #+#    #+#             */
+/*   Updated: 2021/04/03 10:12:55 by echai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "libft.h"
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 17)
+		ft_putstr(ERR_TOO_LITTLE);
+	else
+	{
+		(void)argv;
+	}
+	return (0);
+}
